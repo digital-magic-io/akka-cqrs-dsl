4dbbf535ffe5dbac5245f3aa499054d22d25f015
Dmitri Melnikov <dmitri@digital-magic.io>
2017-11-06 16:17:53 +0200

    Bump akka-actor to 2.5.6 to fix JDK9 build

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   object Versions {
     val scala211  = "2.11.11"
     val scala212  = "2.12.4"
-    val akka      = "2.4.20"
+    val akka      = "2.5.6"
     val scalaTest = "3.0.4"
   }
 
