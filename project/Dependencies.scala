6459b09de70a1d3b0bccf3cf7c0af3f3770da888
Sergei Akhapkin <sergey.akhapkin@gmail.com>
2018-06-21 20:36:18 +0300

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,9 +4,9 @@ object Dependencies {
 
   object Versions {
     val scala211  = "2.11.11"
-    val scala212  = "2.12.4"
-    val akka      = "2.5.6"
-    val scalaTest = "3.0.4"
+    val scala212  = "2.12.6"
+    val akka      = "2.5.13"
+    val scalaTest = "3.0.5"
   }
 
   val dependencies = Seq(
